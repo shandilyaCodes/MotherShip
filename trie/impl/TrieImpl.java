64d57b0c0bfb80eada7ebf32e3dec212800e33e1
Ramendu Shandilya <49898034+shandilyaCodes@users.noreply.github.com>
2022-09-02 22:09:06 +0530

    Create TrieImpl.java

---
+++ trie/impl/TrieImpl.java
@@ -0,0 +1,43 @@
+package trie;
+
+public class TrieImpl {
+
+    private static TrieNode root;
+
+    public TrieImpl() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node.containsKey(word.charAt(i))) {
+                node.put(word.charAt(i), new TrieNode());
+            }
+            node = node.get(word.charAt(i));
+        }
+        node.setEnd();
+    }
+
+    public boolean findWord(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node.containsKey(word.charAt(i))) {
+                return false;
+            }
+            node = node.get(word.charAt(i));
+        }
+        return node.isLast();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (!node.containsKey(prefix.charAt(i))) {
+                return false;
+            }
+            node = node.get(prefix.charAt(i));
+        }
+        return true;
+    }
+}
